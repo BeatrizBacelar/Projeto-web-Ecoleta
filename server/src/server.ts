def8ff550e2b0c88479b3626258671cffaf746cd
BeatrizBacelar <beabacelar18@gmail.com>
2021-01-27 16:05:20 -0300

    Feat(backend): Criação de metódo para pesquisa por ID e método para pesquisa por filtragem

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,17 +14,41 @@ const app = express()
 // GET https://localhost:3333/users = Listar usuários
 // GET https://localhost:3333/users/5 = Buscar os dados do usuário com ID 5
 
+// Resquest Param: Parâmetros que vem na própria rota que identificam um recurso
+// Query Params : são parâmetros que vem na própria rota geralmente opcionais para filtos, paginação
+// Request body : Parâmetros para criação/atualização de informações
+
+const users = [
+    'Diego',
+    'Robson',
+    'Carlos',
+    'Daniel'
+        ];
+
 app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
+    
+const search = String(request.query.search);
 
-// JSON
+const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
 
-    response.json([
-'Diego',
-'Robson',
-'Carlos',
-'Daniel'
-    ]);
+ return  response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+
+app.post('/users', (resquest,response) => {
+    const user = {
+        name: 'Diego',
+        email: 'diego@rocketseat.com.br'
+    };
+  return response.json(user);
 });
 
 app.listen(3333);
