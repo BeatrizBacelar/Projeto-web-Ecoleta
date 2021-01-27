65338c001696376350526e9376d8c920335bf176
BeatrizBacelar <beabacelar18@gmail.com>
2021-01-27 11:34:13 -0300

    Refactor(servidor/backend): Adição de linha de comentário acerca do funcionamento do backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,18 @@ import express from 'express';
 
 const app = express()
 
+// Rota: Endereço completo da requisição
+// Recurso: Qual entidade estamos acessando do sistema 
+
+// GET :  buscar uma ou mais informações do backend
+// POST : criar uma nova informação no backend
+// PUT : atualizar uma informação existente
+// DELETE: remover uma informação do backend
+
+// POST https://localhost:3333/users = Criar um usuário
+// GET https://localhost:3333/users = Listar usuários
+// GET https://localhost:3333/users/5 = Buscar os dados do usuário com ID 5
+
 app.get('/users', (request, response) => {
     console.log('Listagem de usuários');
 
