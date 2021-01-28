ce335f34ca1898af62d03c61343f36c127681a16
BeatrizBacelar <beabacelar18@gmail.com>
2021-01-28 15:07:29 -0300

    Feat(rotas/banco de dados): Adição da rota de listagem de items no controller

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,27 +1,18 @@
 import express from 'express';
-import knex from './database/connection'
 import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/itemsController';
 
+// index: listagem, show: um único registro, create, update, delete
 const routes = express.Router();
 const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (request, response) => {
-    
-    // SELECT * FROM items
-    const items = await knex('items').select('*');
+routes.get('/items', itemsController.index);
 
-    const serializedItems = items.map(item =>{
-        return {
-            id: item.id,
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`,
-        };
-    });
-
-    return response.json(serializedItems)
-  });
 
   routes.post('/points',pointsController.create);
 
   export default routes;
 
+// Service Pattern 
+// Repository Pattern (Data Map)
