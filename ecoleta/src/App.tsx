5887e14ad8fd33cfa0aa4fbdd763f9be5817640a
BeatrizBacelar <beabacelar18@gmail.com>
2021-01-29 01:02:07 -0300

    Feat: Criação do componente Header.

--- ecoleta/src/App.tsx
+++ ecoleta/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+// JSX : Sintaxe de XML dentro do JavaScript
 
 function App() {
   return (
-        <h1>Hello World</h1>
+    <div>
+      <Header />
+
+      <h1>Conteúdo da aplicação</h1>
+    </div>
   );
 }
 
